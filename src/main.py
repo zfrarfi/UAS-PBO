931103f2cf87260bd0ea7909bdfc19261a1ecea3
Fauzannn-AI <zanfauzanx@gmail.com>
2026-05-18 14:24:16 +0700

    main tipis2

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print ("hello Jing!") #tes cuk
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH = 1280
+HEIGHT = 720
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Midnight Cafe")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((67, 67, 67))
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
