47387f55e840ddcc6aa4eb192e4ebab5a95c69ea
Fauzannn-AI <zanfauzanx@gmail.com>
2026-05-19 14:09:43 +0700

    wkwkwk

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ HEIGHT = 720
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Midnight Cafe")
-bg_img = pygame.image.load(r"C:\Users\fauza\Tugas Matkul\semster 2\pbo\Project akhir github\UAS-PBO\assets\image\map\map_lengkap.jpeg")
+bg_img = pygame.image.load("../assets/image/map/map_lengkap.jpeg")
 bg_scaled = pygame.transform.scale(bg_img, (WIDTH, HEIGHT))
 running = True
 #SLDKJWEDGFNEIBF
